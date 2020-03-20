779ae3167e433a52fbdbb88f514b02f8fc601774
Alanna Celentano <alannacelentano@alannas-mbp.attlocal.net>
2020-03-20 16:06:11 -0500

    complete user authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('games/', views.games_index, name='games'),
+    path('accounts/signup/', views.signup, name='signup'),   
 ]
--- thebestbarcade/settings.py
+++ thebestbarcade/settings.py
@@ -90,12 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
@@ -119,3 +113,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/games/'
+LOGOUT_REDIRECT_URL = '/'
