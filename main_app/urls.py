c6164dbf182398c6d6c1e6862c7accd00b042eff
Alanna Celentano <alannacelentano@alannas-mbp.attlocal.net>
2020-03-27 10:12:33 -0500

    fix cards on templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('games/', views.games_index, name='games'),
     path('accounts/signup/', views.signup, name='signup'),  
     path('bars/', views.bars_index, name='bars'),
     path('bars/bars_create', views.BarCreate.as_view(), name='bars_create'),
@@ -12,6 +11,7 @@ urlpatterns = [
     path('bars/<int:bar_id>/', views.bar_details, name='bar_details'),
     path('bars/<int:bar_id>/games_create', views.GameCreate.as_view(), name='games_create'),
     path('bars/<int:pk>/delete', views.BarDelete.as_view(), name='bars_delete'),
+    path('games/', views.games_index, name='games'),
     path('games/<int:game_id>/add_photo/', views.add_photo, name='add_photo'),
     path('games/<int:game_id>', views.game_details, name='game_details'),
     path('games/<int:pk>/delete', views.GameDelete.as_view(), name='games_delete'),
